6a026f97b027236ed0bcc8ea5eca03d150508546
thomas <thomas.treffry@nearform.com>
2021-02-15 07:54:26 -0800

    Set dwm to use the system font

--- .config/fancy-dwm/config.h
+++ .config/fancy-dwm/config.h
@@ -11,7 +11,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = False;     /* Switch view on tag switch */
-static const char *fonts[]          = { "Noto Sans Mono:size=13" };
+static const char *fonts[]          = { "monospace:size=13" };
 static const char dmenufont[]       = "monospace:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
