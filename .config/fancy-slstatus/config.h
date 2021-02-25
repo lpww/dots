ec73f29806ec505966ecbac980077a27d04b628c
thomas <thomas.treffry@nearform.com>
2021-02-25 13:36:39 -0800

    Add spacing around slstatus

--- .config/fancy-slstatus/config.h
+++ .config/fancy-slstatus/config.h
@@ -63,7 +63,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-		{ cpu_perc, "CPU %s%%   ",   NULL  },
+		{ cpu_perc, " CPU %s%%   ",   NULL  },
 		{ ram_perc, "RAM %s%%   ", NULL	      },
 		/*{ disk_free,"[hdd %sB]  | ",      "/" },*/
 		{ datetime, "%s","%a %d %b %R:%S  " }
