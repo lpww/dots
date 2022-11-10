e0bc9603971b1bf54d51f1a5ee8c113fa9de358a
thomas <thomas.treffry@nearform.com>
2022-11-09 16:40:48 -0800

    Add autostart programs

--- .config/fancy-dwm/autostart.sh
+++ .config/fancy-dwm/autostart.sh
@@ -7,23 +7,37 @@ function run {
   fi
 }
 
+# policy kit
+# run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1" #system wide priviledges
+exec /usr/lib/polkit-1/polkit-agent-helper-1 & # system wide priviledges
+
+# status bar / systray
+run slstatus & #dwm status bar
+run parcellite & #clipboard manager
 run "nm-applet" #network manager system tray
 # run "pamac-tray" #system update system tray
 # run "variety" #wallpaper changer
 run "xfce4-power-manager" #power sources of computer and devices
 # run "blueberry-tray" #bluetooth system tray
-# run "/usr/lib/xfce4/notifyd/xfce4-notifyd" #notification service
-# run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1" #system wide priviledges
-picom -b  --config ~/.config/fancy-dwm/picom.conf & #handle transparent windows
-# run "numlockx on" #numlock on at login screen
 # run "volumeicon" #pavucontrol system tray
-run parcellite & #clipboard manager
-run slstatus & #dwm status bar
-sxhkd -c ~/.config/fancy-dwm/sxhkd/sxhkdrc & #load sxhkd (wm agnostic) shortcuts
+
+#notifications
+# run "/usr/lib/xfce4/notifyd/xfce4-notifyd" #notification service
+
+# transparent windows
+run picom -b  --config ~/.config/fancy-dwm/picom.conf & #handle transparent windows
+
+# keyboard
+# run "numlockx on" #numlock on at login screen
+run sxhkd -c ~/.config/fancy-dwm/sxhkd/sxhkdrc & #load sxhkd (wm agnostic) shortcuts
+
+# theme
 run wal -R #restore the last used pywal theme
 #feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
 
-#run applications from startup
+# register screen locker
+run xss-lock slock & # lock screen with slock
 
+# audio
 run pipewire &
 run pipewire-pulse &
