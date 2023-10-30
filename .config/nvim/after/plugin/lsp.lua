194b30fab27dcd1fd8311c330a25861ea38db7aa
thomas <thomas.treffry@nearform.com>
2023-10-30 16:08:14 +0000

    configure lsp servers

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,4 +1,3 @@
-
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
@@ -18,14 +17,8 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'lua_ls', 'clangd', 'gopls', 'graphql', 'zls', 'tailwindcss'},
-  handlers = {
-    lsp_zero.default_setup,
-    lua_ls = function()
-      local lua_opts = lsp_zero.nvim_lua_ls()
-      require('lspconfig').lua_ls.setup(lua_opts)
-    end,
-  }
+  ensure_installed = {'tsserver', 'gopls', 'zls'},
+  handlers = { lsp_zero.default_setup }
 })
 
 local cmp = require('cmp')
