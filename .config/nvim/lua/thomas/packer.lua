96ffea364200bc8d557aa606887d27d5e370f7b5
thomas <thomas.treffry@nearform.com>
2023-11-05 14:57:59 +0000

    remove harpoon

--- .config/nvim/lua/thomas/packer.lua
+++ .config/nvim/lua/thomas/packer.lua
@@ -17,8 +17,6 @@ return require('packer').startup(function(use)
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
 
-  use('theprimeagen/harpoon')
-
   use('mbbill/undotree')
 
   use('tpope/vim-commentary')
