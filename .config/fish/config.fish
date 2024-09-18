87952905e6fdbd1d725397a175bac85668b8e864
thomas <thomas.treffry@yld.io>
2024-09-18 10:31:11 +0100

    Improve config.fish comment

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 # everything below this only executes in interactive mode
 status is-interactive || exit
 
-# install fisher if it's not found
+# install fisher and plugins if not found
 if not functions -q fisher
 	  curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
 end
