e94488f0dc8a01dc082a5363ade7c77b31fe0d45
thomas <thomas.treffry@yld.io>
2024-09-18 16:13:47 +0100

    Refactor env vars and user path out of fish_variables

---
+++ .config/fish/conf.d/00-env.fish
@@ -0,0 +1,23 @@
+# set env vars
+
+# xdg
+# https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+set -q XDG_CONFIG_HOME; or set -gx XDG_CONFIG_HOME $HOME/.config
+set -q XDG_DATA_HOME; or set -gx XDG_DATA_HOME $HOME/.local/share
+set -q XDG_STATE_HOME; or set -gx XDG_STATE_HOME $HOME/.local/state
+set -q XDG_CACHE_HOME; or set -gx XDG_CACHE_HOME $HOME/.cache
+mkdir -p $XDG_CONFIG_HOME $XDG_DATA_HOME $XDG_STATE_HOME $XDG_CACHE_HOME
+
+# fish
+set -q __fish_cache_dir; or set -gx __fish_cache_dir $XDG_CACHE_HOME/fish
+test -d $__fish_cache_dir; or mkdir -p $__fish_cache_dir
+
+# editor
+set -gx PAGER less
+set -gx EDITOR nvim
+
+# ripgrep
+set -gx RIPGREP_CONFIG_PATH $XDG_CONFIG_HOME/ripgrep/.ripgreprc
+
+# fish-nvm
+set -gx nvm_default_version lts
---
+++ .config/fish/conf.d/01-path.fish
@@ -0,0 +1,4 @@
+fish_add_path --prepend /opt/homebrew/bin
+fish_add_path --prepend /usr/local/bin
+fish_add_path --prepend $HOME/bin
+fish_add_path --prepend $HOME/go/bin
