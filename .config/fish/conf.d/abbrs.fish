9e6286dfeba25b03856328be02019e8cc5c87d87
thomas <thomas.treffry@yld.io>
2024-09-18 16:33:59 +0100

    Move abbrs from fish_variables to conf.d/abbrs.fish

---
+++ .config/fish/conf.d/abbrs.fish
@@ -0,0 +1,73 @@
+abbr -a -- xiu 'sudo xbps-install -Su' # imported from a universal variable, see `help abbr`
+abbr -a -- gd 'git diff' # imported from a universal variable, see `help abbr`
+abbr -a -- xis 'sudo xbps-install -S' # imported from a universal variable, see `help abbr`
+abbr -a -- gc 'git commit' # imported from a universal variable, see `help abbr`
+abbr -a -- nli 'npm link' # imported from a universal variable, see `help abbr`
+abbr -a -- xdd 'xrandr --auto && xrandr --output HDMI1 --off && xrandr --output DP2 --off && xrandr --output eDP1 --auto --primary' # imported from a universal variable, see `help abbr`
+abbr -a -- dvp 'docker volume prune' # imported from a universal variable, see `help abbr`
+abbr -a -- yre 'rm -rf node_modules && yarn install' # imported from a universal variable, see `help abbr`
+abbr -a -- gcl git\ branch\ --merged\ \|\ grep\ -v\ \'\\\*\\\|master\\\|develop\'\ \|\ xargs\ -n\ 1\ git\ branch\ -d # imported from a universal variable, see `help abbr`
+abbr -a -- dcp 'docker container prune' # imported from a universal variable, see `help abbr`
+abbr -a -- xld 'xrandr --output eDP1 --off && xrandr --output DP1-1 --primary && xset s 600' # imported from a universal variable, see `help abbr`
+abbr -a -- bat 'echo 41%' # imported from a universal variable, see `help abbr`
+abbr -a -- gcb 'git rev-parse --abbrev-ref HEAD' # imported from a universal variable, see `help abbr`
+abbr -a -- wttr curl\ http://wttr.in/\\\~ # imported from a universal variable, see `help abbr`
+abbr -a -- ni 'npm install' # imported from a universal variable, see `help abbr`
+abbr -a -- nt 'npm test' # imported from a universal variable, see `help abbr`
+abbr -a -- xle 'xrandr --output eDP1 --auto --primary && xset s 600' # imported from a universal variable, see `help abbr`
+abbr -a -- gch 'git checkout' # imported from a universal variable, see `help abbr`
+abbr -a -- xqf 'xbps-query -f' # imported from a universal variable, see `help abbr`
+abbr -a -- gf 'git fetch' # imported from a universal variable, see `help abbr`
+abbr -a -- hl 'handlr list' # imported from a universal variable, see `help abbr`
+abbr -a -- gk 'gitk --all' # imported from a universal variable, see `help abbr`
+abbr -a -- dsp 'docker system prune' # imported from a universal variable, see `help abbr`
+abbr -a -- ns 'npm start' # imported from a universal variable, see `help abbr`
+abbr -a -- gfo fetch_branch # imported from a universal variable, see `help abbr`
+abbr -a -- ga 'git add' # imported from a universal variable, see `help abbr`
+abbr -a -- piauk 'sudo openvpn --config /etc/openvpn/client/UK_London.conf' # imported from a universal variable, see `help abbr`
+abbr -a -- nuli 'npm unlink' # imported from a universal variable, see `help abbr`
+abbr -a -- gr 'git reflog' # imported from a universal variable, see `help abbr`
+abbr -a -- gb 'git branch' # imported from a universal variable, see `help abbr`
+abbr -a -- gs 'git status' # imported from a universal variable, see `help abbr`
+abbr -a -- nml 'nmcli d wifi list' # imported from a universal variable, see `help abbr`
+abbr -a -- ng 'npm list -g --depth=apply' # imported from a universal variable, see `help abbr`
+abbr -a -- zil 'zig init-lib' # imported from a universal variable, see `help abbr`
+abbr -a -- gsh 'git show' # imported from a universal variable, see `help abbr`
+abbr -a -- gx 'gitx --all' # imported from a universal variable, see `help abbr`
+abbr -a -- nre 'rm -rf node_modules && npm install' # imported from a universal variable, see `help abbr`
+abbr -a -- hg 'handlr get' # imported from a universal variable, see `help abbr`
+abbr -a -- hs 'handlr set' # imported from a universal variable, see `help abbr`
+abbr -a -- nci 'rm -rf node_modules && npm ci' # imported from a universal variable, see `help abbr`
+abbr -a -- piauseast 'sudo openvpn --config /etc/openvpn/client/US_East.conf' # imported from a universal variable, see `help abbr`
+abbr -a -- nmu 'nmcli c up' # imported from a universal variable, see `help abbr`
+abbr -a -- nr 'npm run' # imported from a universal variable, see `help abbr`
+abbr -a -- pro-rds-develop 'xbstunnel -P pro -p 9400' # imported from a universal variable, see `help abbr`
+abbr -a -- nv 'npm version' # imported from a universal variable, see `help abbr`
+abbr -a -- zb 'zig build' # imported from a universal variable, see `help abbr`
+abbr -a -- nmd 'nmcli c down' # imported from a universal variable, see `help abbr`
+abbr -a -- piafr 'sudo openvpn --config /etc/openvpn/client/France.conf' # imported from a universal variable, see `help abbr`
+abbr -a -- glch 'git log -n 1 --pretty=format:%H' # imported from a universal variable, see `help abbr`
+abbr -a -- xde 'xrandr --output DP1-1 --auto && xrandr --output DP1-2 --auto --right-of DP1-1 && xset s 600' # imported from a universal variable, see `help abbr`
+abbr -a -- v vim # imported from a universal variable, see `help abbr`
+abbr -a -- xa 'xrandr --auto && xset s on && xset s 600' # imported from a universal variable, see `help abbr`
+abbr -a -- xds 'xrandr --output eDP1 --off && xrandr --output HDMI1 --auto --primary && xrandr --output DP2 --off' # imported from a universal variable, see `help abbr`
+abbr -a -- piauswest 'sudo openvpn --config /etc/openvpn/client/US_West.conf' # imported from a universal variable, see `help abbr`
+abbr -a -- xqd 'xbps-query -R' # imported from a universal variable, see `help abbr`
+abbr -a -- xql 'xbps-query -l' # imported from a universal variable, see `help abbr`
+abbr -a -- xqs 'xbps-query -s' # imported from a universal variable, see `help abbr`
+abbr -a -- xrr 'sudo xbps-remove -R' # imported from a universal variable, see `help abbr`
+abbr -a -- ya 'yarn add' # imported from a universal variable, see `help abbr`
+abbr -a -- yi 'yarn install' # imported from a universal variable, see `help abbr`
+abbr -a -- yr 'yarn run' # imported from a universal variable, see `help abbr`
+abbr -a -- ys 'yarn start' # imported from a universal variable, see `help abbr`
+abbr -a -- yt 'yarn test' # imported from a universal variable, see `help abbr`
+abbr -a -- xqrs 'xbps-query -Rs' # imported from a universal variable, see `help abbr`
+abbr -a -- gl 'git log' # imported from a universal variable, see `help abbr`
+abbr -a -- bu 'brightness up' # imported from a universal variable, see `help abbr`
+abbr -a -- zie 'zig init-exe' # imported from a universal variable, see `help abbr`
+abbr -a -- bd 'brightness down' # imported from a universal variable, see `help abbr`
+abbr -a -- zbt 'zig build test' # imported from a universal variable, see `help abbr`
+abbr -a -- gp 'git pull' # imported from a universal variable, see `help abbr`
+abbr -a -- ho 'handlr open' # imported from a universal variable, see `help abbr`
+abbr -a -- zbr 'zig build run' # imported from a universal variable, see `help abbr`
+abbr -a -- nu 'npm update' # imported from a universal variable, see `help abbr`
