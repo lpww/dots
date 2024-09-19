98915bd796fd420a51b6957e399f9ac7674cd54e
thomas <thomas.treffry@yld.io>
2024-09-19 14:58:28 +0100

    Add markdown support to neovim

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -17,7 +17,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'ts_ls'},
+  ensure_installed = {'ts_ls', 'marksman'},
   handlers = { lsp_zero.default_setup }
   })
 
--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "go", "zig", "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query" },
+  -- A list of parser names, or "all" (the listed parsers should always be installed)
+  ensure_installed = { "go", "zig", "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
