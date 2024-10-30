95d1ae27266a9c1476c508e54d0440bb11ca866b
thomas <thomas.treffry@yld.io>
2024-10-30 08:58:23 +0000

    Switch nvim colorscheme to use pywal

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,5 +1,5 @@
 function SetColor(color)
-	color = color or "leaf"
+	color = color or "neopywal"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- .config/nvim/lua/thomas/packer.lua
+++ .config/nvim/lua/thomas/packer.lua
@@ -22,7 +22,7 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  use({ 'daschw/leaf.nvim', as = 'leaf' })
+  use { "RedsXDD/neopywal.nvim", as = "neopywal" }
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
