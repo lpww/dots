2b9bdb038d65eaccf7a8e0f45ab7c34f1aea4393
thomas <thomas.treffry@yld.io>
2024-10-30 08:59:05 +0000

    Enable quit keybind in dwm

--- .config/fancy-dwm/config.h
+++ .config/fancy-dwm/config.h
@@ -87,11 +87,11 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	/*{ MODKEY,                       XK_Tab,    view,           {0} },*/
-{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+  { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-{ MODKEY,                       XK_space,  setlayout,      {0} },
+  { MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
@@ -99,7 +99,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-/* { MODKEY|ShiftMask,             XK_q,      quit,           {0} }, */
+  { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
   TAGKEYS(                        XK_1,                      0)
   TAGKEYS(                        XK_2,                      1)
   TAGKEYS(                        XK_3,                      2)
