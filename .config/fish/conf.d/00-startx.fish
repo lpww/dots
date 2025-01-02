883c9b1889062db2c5a9c6a97221971a99e2da53
thomas <thomas.treffry@yld.io>
2025-01-02 15:23:53 +0000

    Add startx/wayland config file

--- .config/fish/conf.d/00-startx.fish
+++ .config/fish/conf.d/00-startx.fish
@@ -2,3 +2,15 @@
 if command -q startx && test (tty) = "/dev/tty1"
   pgrep dwm || startx
 end
+
+# run wayland (via river compositor) if we are in tty1 and there is no other compositor running
+if test -z $WAYLAND_DISPLAY && test (tty) = "/dev/tty1"
+  # keyring daemon for secrets
+  eval $(gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh | sed 's/^\(.*\)/export \1/g')
+
+  # run hazy in a dbus session with alacritty as the open window
+  # exec dbus-run-session /home/thomas/code/hazy/zig-out/bin/hazy ghostty
+
+  # run river in a dbus session
+  # exec dbus-run-session /usr/bin/river
+end
