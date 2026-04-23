014bfdb43abea8cf412d2612cd01ef5e94aa663f
thomas <thomas.treffry@yld.io>
2026-04-23 11:28:44 +0100

    Add telescope go to ref and go to imp mapping

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -4,3 +4,5 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set('n', 'gi', builtin.lsp_implementations, {})
+vim.keymap.set('n', 'gr', builtin.lsp_references, {})
